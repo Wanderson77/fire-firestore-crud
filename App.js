1dc6656a8d2018e8f4b6b74bd43294d22a32ad59
wanderson-aldo <wanderson.aldo@gmail.com>
2024-12-25 13:44:13 -0300

    fb-firestore-crud

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// #2 Integration of Firebase Cloud Firestore Database with React Native App
+// https://aboutreact.com/react-native-firebase-cloud-firestore-db
 
-export default function App() {
+import 'react-native-gesture-handler';
+
+import * as React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import HomeScreen from './pages/HomeScreen';
+import RegisterUser from './pages/RegisterUser';
+import UpdateUser from './pages/UpdateUser';
+import ViewAllUser from './pages/ViewAllUser';
+import ViewUser from './pages/ViewUser';
+import DeleteUser from './pages/DeleteUser';
+import RealTimeAddUpdateUser from './pages/RealTimeAddUpdateUser';
+import AddOrderSummary from './pages/AddOrderSummary';
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="HomeScreen"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#03A89E', //Set Header color
+          },
+          headerTintColor: '#fff', //Set Header text color
+          headerTitleStyle: {
+            fontWeight: 'bold', //Set Header text style
+          },
+        }}>
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{title: 'Home'}}
+        />
+        <Stack.Screen
+          name="RegisterUser"
+          component={RegisterUser}
+          options={{title: 'Register'}}
+        />
+        <Stack.Screen
+          name="UpdateUser"
+          component={UpdateUser}
+          options={{title: 'Update'}}
+        />
+        <Stack.Screen
+          name="ViewAllUser"
+          component={ViewAllUser}
+          options={{title: 'View All'}}
+        />
+        <Stack.Screen
+          name="ViewUser"
+          component={ViewUser}
+          options={{title: 'View'}}
+        />
+        <Stack.Screen
+          name="DeleteUser"
+          component={DeleteUser}
+          options={{title: 'Delete'}}
+        />
+        <Stack.Screen
+          name="RealTimeAddUpdateUser"
+          component={RealTimeAddUpdateUser}
+          options={{title: 'Real Time Updates'}}
+        />
+        <Stack.Screen
+          name="AddOrderSummary"
+          component={AddOrderSummary}
+          options={{title: 'Add Order Summary'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
